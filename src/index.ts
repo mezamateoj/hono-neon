01e3c1298e0fa4e7043e0bfe126269ccefed875a
Mateo Meza <mmeza@microsystem.cl>
2024-05-18 12:32:05 -0400

    Add code template of hono

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,35 @@
 import { Hono } from 'hono'
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
+import { products } from './db/schema';
 
-type Bindings = {
-  [key in keyof CloudflareBindings]: CloudflareBindings[key]
-}
+export type Env = {
+  DATABASE_URL: string;
+};
 
-const app = new Hono<{ Bindings: Bindings }>()
+const app = new Hono<{ Bindings: Env }>()
+app.notFound((c) => {
+  return c.text('Not Found', 404)
+})
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.json({
+    message: 'Hello from hono, try /products'
+  })
 })
+
+app.get('/products', async (c) => {
+  const sql = neon(c.env.DATABASE_URL);
+  const db = drizzle(sql);
+  const result = await db.select().from(products);
+  return c.json({ result });
+})
+
+
+app.onError((error, c) => {
+  console.log(error)
+  return c.json({ error }, 400)
+})
+
 
 export default app
